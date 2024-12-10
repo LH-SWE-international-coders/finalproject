9619afaddfd4209089ef8491da402ca629bc15da
koenaadg <koenraad.goddefroy@student.kuleuven.be>
2024-12-10 10:30:06 +0000

    Changed global css of default buttons, background, and tabs

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,21 +10,21 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "button-default",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          "button-destructive",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "button-outline",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "button-secondary",
+        ghost: "button-ghost",
+        link: "button-link",
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        default: "button-default-size",
+        sm: "button-sm-size",
+        lg: "button-lg-size",
+        icon: "button-icon-size",
       },
     },
     defaultVariants: {
